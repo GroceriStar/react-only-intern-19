c5b74eda21a5e6fafbfa4a84b4e6fa48d27d0705
guruduttperi <thegdaproject@gmail.com>
2018-05-29 22:08:07 +0530

    Git Pushed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
-
+import './App.css';
 
 class App extends Component {
+
+  getDayName(number){
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[number];
+  }
+
+  getMonthName(number){
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return months[number];
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <div> Hello World </div>
-      </div>
-    );
+
+    // Get Current Day
+    let dateObject = new Date();
+    let today = dateObject.getDay();
+    let month = dateObject.getMonth();
+    let date = dateObject.getDate();
+
+    // If Weekend
+    if(today === 6 || today === 0)
+      return ( <div class = "Mid">
+               <div> Today is {date} of {this.getMonthName(month)}, {this.getDayName(today)} </div>
+               <div>You can watch TV all day today!</div>
+
+             </div>
+             );
+      else {
+        return ( <div class = "NMid">
+                <div> Today is {date} of {this.getMonthName(month)}, {this.getDayName(today)} </div>
+                 <div>You should go to work today!</div>
+               </div>
+               );
+      }
   }
 }
 
